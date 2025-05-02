5f2d21d9010b6a4adba06365535b675211a48c60
Benjamin Amayo <amayobenjamin16@gmail.com>
2025-05-02 17:03:16 -0500

    prueba2

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-    authStrategy: new LocalAuth(),
+    authStrategy: new LocalAuth(), // Guarda sesión en .wwebjs_auth
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: ['--no-sandbox', '--disable-setuid-sandbox'] // Necesario para Railway o Docker
     }
 });
 
 client.on('qr', (qr) => {
-    console.log('Escanea este código QR con tu WhatsApp:');
+    console.log('📱 Escanea este código QR con tu WhatsApp:');
     qrcode.generate(qr, { small: true });
 });
 
