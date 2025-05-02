7b3926aa331a9b27f9602100708bca96aba4762f
Benjamin Amayo <amayobenjamin16@gmail.com>
2025-05-02 16:52:38 -0500

    sisi

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const qrcode = require('qrcode-terminal');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: { headless: true }
+    puppeteer: {
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
 });
 
 client.on('qr', (qr) => {
